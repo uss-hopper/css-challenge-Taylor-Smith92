dab5316bc855b56f322cffd8abd6e300963135ee
Taylor Smith <taylorleesmith92@gmail.com>
2019-02-04 10:01:40 -0700

    updates

--- index.php
+++ index.php
@@ -84,7 +84,7 @@
 			</section>
 
 			<!-- CONTACT FORM -->
-			<section class="footer">
+			<section class="form-section">
 				<div class="container">
 					<div class="form-wrapper">
 						<h2>Contact Me! I <3 to fix printers!</h2>
